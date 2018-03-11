2466aa9aa903496d875bab0f6416441934548f49
Tony Cetuk <cetukt@gmail.com>
2018-03-11 18:42:50 -0400

    Added unit tests for filter reducer

--- src/reducers/filter-reducer.js
+++ src/reducers/filter-reducer.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import moment from 'moment';
 
 const DEFAULT_FILTERS = {
-  description: '',
+  text: '',
   sortBy: 'date',
   startDate: moment().startOf('month'),
   endDate: moment().endOf('month')
@@ -11,24 +11,32 @@ const DEFAULT_FILTERS = {
 export default function filterReducer(state = DEFAULT_FILTERS, action) {
   switch (action.type) {
     case 'FILTER_TEXT': {
-      const filters = _.cloneDeep(state);
-      filters.text = action.payload.text || DEFAULT_FILTERS.text;
-      return filters;
+      return Object.assign(
+        {},
+        state.filters,
+        { text: action.payload.text || DEFAULT_FILTERS.text }
+      );
     }
     case 'SORT_BY_TYPE': {
-      const filters = _.cloneDeep(state);
-      filters.sortBy = action.payload.sortBy || DEFAULT_FILTERS.sortBy;
-      return filters;
+      return Object.assign(
+        {},
+        state,
+        { sortBy: action.payload.sortBy || DEFAULT_FILTERS.sortBy }
+      );
     }
     case 'SET_START_DATE': {
-      const filters = _.cloneDeep(state);
-      filters.startDate = action.payload.startDate || DEFAULT_FILTERS.startDate;
-      return filters;
+      return Object.assign(
+        {},
+        state,
+        { startDate: action.payload.startDate || DEFAULT_FILTERS.startDate }
+      );
     }
     case 'SET_END_DATE': {
-      const filters = _.cloneDeep(state);
-      filters.endDate = action.payload.endDate || DEFAULT_FILTERS.endDate;
-      return filters;
+      return Object.assign(
+        {},
+        state,
+        { endDate: action.payload.endDate || DEFAULT_FILTERS.endDate }
+      );
     }
     default:
       return state;
--- test/reducers/filter-reducer-test.js
+++ test/reducers/filter-reducer-test.js
@@ -4,14 +4,90 @@ import moment from 'moment';
 import filterReducer from '../../src/reducers/filter-reducer';
 
 describe('filter reducer', () => {
+  let testActionData;
+
+  beforeEach(() => {
+    testActionData = {
+      type: undefined,
+      payload: {}
+    };
+  });
+
   it('creates initial state', () => {
     const result = filterReducer(undefined, { type: '@@INIT' });
     expect(result).to.exist;
     expect(result).to.deep.equal({
-      description: '',
+      text: '',
       sortBy: 'date',
       startDate: moment().startOf('month'),
       endDate: moment().endOf('month')
     });
+  });
+
+  it('filters on text', () => {
+    testActionData.type = 'FILTER_TEXT';
+    testActionData.payload.text = 'value1';
+    const result = filterReducer(undefined, testActionData);
+    expect(result).to.have.property('text', 'value1');
+  });
+
+  it('filters on text with default', () => {
+    testActionData.type = 'FILTER_TEXT';
+    testActionData.payload.text = null;
+    const result = filterReducer(undefined, testActionData);
+    expect(result).to.have.property('text', '');
+  });
+
+  it('sorts by amount', () => {
+    testActionData.type = 'SORT_BY_TYPE';
+    testActionData.payload.sortBy = 'amount';
+    const result = filterReducer({ sortBy: '' }, testActionData);
+    expect(result).to.have.property('sortBy', 'amount');
+  });
+
+  it('sorts by date', () => {
+    testActionData.type = 'SORT_BY_TYPE';
+    testActionData.payload.sortBy = 'date';
+    const result = filterReducer({ sortBy: '' }, testActionData);
+    expect(result).to.have.property('sortBy', 'date');
+  });
+
+  it('sort by with default', () => {
+    testActionData.type = 'SORT_BY_TYPE';
+    testActionData.payload.sortBy = null;
+    const result = filterReducer(undefined, testActionData);
+    expect(result).to.have.property('sortBy', 'date');
+  });
+
+  it('sets start date', () => {
+    const currentTimestamp = moment.utc();
+    testActionData.type = 'SET_START_DATE';
+    testActionData.payload.startDate = currentTimestamp;
+    const result = filterReducer(undefined, testActionData);
+    expect(result).to.have.property('startDate', currentTimestamp);
+  });
+
+  it('sets start date with default', () => {
+    testActionData.type = 'SET_START_DATE';
+    testActionData.payload.startDate = null;
+    const result = filterReducer(undefined, testActionData);
+    expect(result.startDate).to.exist;
+    expect(result.startDate.toISOString()).to.equal(moment().startOf('month').toISOString());
+  });
+
+  it('sets end date', () => {
+    const currentTimestamp = moment.utc();
+    testActionData.type = 'SET_END_DATE';
+    testActionData.payload.endDate = currentTimestamp;
+    const result = filterReducer(undefined, testActionData);
+    expect(result).to.have.property('endDate', currentTimestamp);
+  });
+
+  it('sets end date with default', () => {
+    testActionData.type = 'SET_END_DATE';
+    testActionData.payload.endDate = null;
+    const result = filterReducer(undefined, testActionData);
+    expect(result.endDate).to.exist;
+    expect(result.endDate.toISOString()).to.equal(moment().endOf('month').toISOString());
   });
 });
